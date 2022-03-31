4438c9dd73b5e6c476cae6ddf1560b82b9120610
慕 <37036526+charlotte-mu@users.noreply.github.com>
2022-03-31 17:29:18 +0800

    Add files via upload

---
+++ rtl/geofence.v
@@ -0,0 +1,659 @@
+/******************************************************************/
+//MODULE:		geofence
+//FILE NAME:	geofence.v
+//VERSION:		1.0
+//DATE:			march,2021
+//AUTHOR: 		charlotte-mu
+//CODE TYPE:	RTL
+//DESCRIPTION:	2021 University/College IC Design Contest
+//
+//MODIFICATION HISTORY:
+// VERSION Date Description
+// 1.0 03/24/2021 testpattern all pass
+/******************************************************************/
+module geofence ( clk,reset,X,Y,valid,is_inside);
+input clk;
+input reset;
+input [9:0] X;
+input [9:0] Y;
+output reg valid;
+output is_inside;
+
+reg [9:0]x_data[6:0];
+reg [9:0]y_data[6:0];
+reg [2:0]data_mode;
+reg signed [10:0]ax,ay,bx,by;
+reg signed [10:0]ax_next,ay_next,bx_next,by_next;
+reg fit,fit_next;
+
+reg signed [21:0]ax_by,ay_bx;
+reg signed [21:0]ax_by_next,ay_bx_next;
+
+wire signed [21:0]ans;
+reg [4:0]count,count_next;
+reg [4:0]fsm,fsm_next;
+
+reg signed [10:0]xinA,xinB;
+wire signed [21:0]xans;
+assign xans = xinA * xinB;
+
+wire [4:0]count_ans,count2;
+assign count_ans = count + 5'd1;
+assign count2 = count + 5'd2;
+
+reg [21:0]sainA,sainB;
+wire [21:0]saans;
+assign saans = sainA - sainB;
+
+reg [9:0]sbinA,sbinB;
+wire [10:0]sbans;
+assign sbans = sbinA - sbinB;
+
+assign is_inside = ~fit;
+
+
+always@(posedge clk,posedge reset)
+if(reset)
+begin
+	x_data[0] <= 10'd0;
+	x_data[1] <= 10'd0;
+	x_data[2] <= 10'd0;
+	x_data[3] <= 10'd0;
+	x_data[4] <= 10'd0;
+	x_data[5] <= 10'd0;
+	x_data[6] <= 10'd0;
+	y_data[0] <= 10'd0;
+	y_data[1] <= 10'd0;
+	y_data[2] <= 10'd0;
+	y_data[3] <= 10'd0;
+	y_data[4] <= 10'd0;
+	y_data[5] <= 10'd0;
+	y_data[6] <= 10'd0;
+	
+	ax <= 11'd0;
+	ay <= 11'd0;
+	bx <= 11'd0;
+	by <= 11'd0;
+	count <= 5'd0;
+	fsm <= 5'd0;
+	ax_by <= 22'd0;
+	ay_bx <= 22'd0;
+	fit <= 1'b0; 
+end
+else
+begin
+	ax <= ax_next;
+	ay <= ay_next;
+	bx <= bx_next;
+	by <= by_next;
+	count <= count_next;
+	fsm <= fsm_next;
+	ax_by <= ax_by_next;
+	ay_bx <= ay_bx_next;
+	fit <= fit_next;
+	
+	case(data_mode)	//synopsys parallel_case
+		default:
+		begin
+			x_data[0] <= x_data[0];
+			x_data[1] <= x_data[1];
+			x_data[2] <= x_data[2];
+			x_data[3] <= x_data[3];
+			x_data[4] <= x_data[4];
+			x_data[5] <= x_data[5];
+			x_data[6] <= x_data[6];
+			             
+			y_data[0] <= y_data[0];
+			y_data[1] <= y_data[1];
+			y_data[2] <= y_data[2];
+			y_data[3] <= y_data[3];
+			y_data[4] <= y_data[4];
+			y_data[5] <= y_data[5];
+			y_data[6] <= y_data[6];
+		end
+		3'd1:
+		begin
+			x_data[0] <= x_data[1];
+			x_data[1] <= x_data[2];
+			x_data[2] <= x_data[3];
+			x_data[3] <= x_data[4];
+			x_data[4] <= x_data[5];
+			x_data[5] <= x_data[6];
+			x_data[6] <= X;
+			
+			y_data[0] <= y_data[1];
+			y_data[1] <= y_data[2];
+			y_data[2] <= y_data[3];
+			y_data[3] <= y_data[4];
+			y_data[4] <= y_data[5];
+			y_data[5] <= y_data[6];
+			y_data[6] <= Y;
+		end
+		3'd2:
+		begin
+			x_data[0] <= 10'd0;
+			x_data[1] <= 10'd0;
+			x_data[2] <= 10'd0;
+			x_data[3] <= 10'd0;
+			x_data[4] <= 10'd0;
+			x_data[5] <= 10'd0;
+			x_data[6] <= 10'd0;
+			             
+			y_data[0] <= 10'd0;
+			y_data[1] <= 10'd0;
+			y_data[2] <= 10'd0;
+			y_data[3] <= 10'd0;
+			y_data[4] <= 10'd0;
+			y_data[5] <= 10'd0;
+			y_data[6] <= 10'd0;
+		end
+		3'd3:
+		begin
+			x_data[0] <= x_data[0];
+			x_data[1] <= x_data[6];
+			x_data[2] <= x_data[1];
+			x_data[3] <= x_data[2];
+			x_data[4] <= x_data[3];
+			x_data[5] <= x_data[4];
+			x_data[6] <= x_data[5];
+			
+			y_data[0] <= y_data[0];
+			y_data[1] <= y_data[6];
+			y_data[2] <= y_data[1];
+			y_data[3] <= y_data[2];
+			y_data[4] <= y_data[3];
+			y_data[5] <= y_data[4];
+			y_data[6] <= y_data[5];
+		end
+		3'd4:
+		begin
+			x_data[count] <= x_data[count_ans];
+			x_data[count_ans] <= x_data[count];
+			
+			y_data[count] <= y_data[count_ans];
+			y_data[count_ans] <= y_data[count];
+		end
+	endcase
+end
+
+always@(*)
+begin
+	case(fsm)
+		5'd0:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd1;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = fsm;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+			
+			if(count == 5'd6)
+			begin
+				fsm_next = 5'd1;
+				count_next = 5'd2;
+			end
+			else
+			begin
+				count_next = count_ans;
+			end
+		end
+		5'd1:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = sbans;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd8;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = x_data[count];
+			sbinB = x_data[1];
+		end
+		5'd8:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = sbans;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd9;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = y_data[count];
+			sbinB = y_data[1];
+		end
+		5'd9:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = sbans;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd10;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = x_data[count_ans];
+			sbinB = x_data[1];
+		end
+		5'd10:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = sbans;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd2;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA =  y_data[count_ans];
+			sbinB =  y_data[1];
+		end
+		5'd2:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = xans;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd3;
+			fit_next = fit;
+			xinA = ax;
+			xinB = by;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+		5'd3:
+		begin
+			valid = 1'b0;
+			data_mode = 2'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = xans;
+			count_next = count;
+			fsm_next = 5'd4;
+			fit_next = fit;
+			xinA = ay;
+			xinB = bx;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+		5'd4:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = fsm;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = ax_by;
+			sainB = ay_bx;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+			if(saans[21] == 1'b0)
+			begin
+				fsm_next = 5'd5;
+			end
+			else
+			begin
+				fsm_next = 5'd7;
+			end
+		end
+		5'd5:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd4;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd7;
+			fit_next = 1'b1;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+		5'd7:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fit_next = fit;
+			if(count == 5'd5)
+			begin
+				if(fit == 1'b1)
+				begin
+					fsm_next = 5'd1;
+					count_next = 5'd2;
+					fit_next = 1'b0;
+				end
+				else
+				begin
+					count_next = 5'd0;
+					fit_next = 1'b0;
+					fsm_next = 5'd11;
+				end
+			end
+			else
+			begin
+				fsm_next = 5'd1;
+				count_next = count_ans;
+			end
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+		//-------------------------------------------------
+		5'd11:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = sbans;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd12;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = x_data[1];
+			sbinB = x_data[0];
+		end
+		5'd12:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = sbans;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd13;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = y_data[1];
+			sbinB = y_data[0];
+		end
+		5'd13:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = sbans;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd14;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = x_data[2];
+			sbinB = x_data[0];
+		end
+		5'd14:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = sbans;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd15;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA =  y_data[2];
+			sbinB =  y_data[0];
+		end
+		5'd15:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = xans;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd16;
+			fit_next = fit;
+			xinA = ax;
+			xinB = by;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+		5'd16:
+		begin
+			valid = 1'b0;
+			data_mode = 2'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = xans;
+			count_next = count;
+			fsm_next = 5'd17;
+			fit_next = fit;
+			xinA = ay;
+			xinB = bx;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+		5'd17:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd18;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = ax_by;
+			sainB = ay_bx;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+			if(saans[21] == 1'b0)
+			begin
+				fit_next = 1'b1;
+			end
+		end
+		5'd18:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd3;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count_ans;
+			if(count == 5'd5)
+			begin
+				fsm_next = 5'd19;
+			end
+			else
+			begin
+				fsm_next = 5'd11;
+			end
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+		5'd19:
+		begin
+			valid = 1'b1;
+			data_mode = 3'd2;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = 5'd20;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+		5'd20:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd1;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = 5'd1;
+			fsm_next = 5'd0;
+			fit_next = 1'b0;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+		default:
+		begin
+			valid = 1'b0;
+			data_mode = 3'd0;
+			ax_next = ax;
+			ay_next = ay;
+			bx_next = bx;
+			by_next = by;
+			ax_by_next = ax_by;
+			ay_bx_next = ay_bx;
+			count_next = count;
+			fsm_next = fsm;
+			fit_next = fit;
+			xinA = 11'd0;
+			xinB = 11'd0;
+			sainA = 22'd0;
+			sainB = 22'd0;
+			sbinA = 10'd0;
+			sbinB = 10'd0;
+		end
+	endcase
+end
+
+
+
+
+endmodule
+
+                                                                                                                                       
--- simulation/tb.sv
+++ simulation/tb.sv
@@ -1,51 +1,46 @@
 `timescale 1ns/10ps
-`define CYCLE      20.0  
-`define SDFFILE    "./SME_syn.sdf"
+`define CYCLE      30.0  
+`define SDFFILE    "./geofence_syn.sdf"
 `define End_CYCLE  1000000
-`define PAT        "Etestdata.txt"
-module testfixture();
+`define PAT        "univ.data"
 
-integer linedata;
-integer char_count;
-//reg [128:0] data;
-string data;
-string strnum;
-string patnum;
-string strdata;
-string goldmatch_s;
-string goldmatch_index_s;
+module testfixture();
+integer fd;
+integer objnum;
+integer obj_isin;
+integer charcount;
+integer pass=0;
+integer fail=0;
+string line;
+reg [5:0] npoint;
+reg [9:0] X;
+reg [9:0] Y;
 
 reg clk = 0;
 wire valid;
 reg reset =0;
-reg [7:0] chardata;
-reg isstring=0;
-reg ispattern=0;
-wire match;
-wire [4:0] match_index;
-SME u_SME (.clk(clk),
-           .reset(reset),
-           .chardata(chardata),
-           .isstring(isstring),
-           .ispattern(ispattern),
-           .valid(valid),
-           .match(match),
-           .match_index(match_index));
+wire is_inside;
+geofence u_geofence(.clk(clk),
+        .reset(reset),
+        .X(X),
+        .Y(Y),
+        .valid(valid),
+        .is_inside(is_inside));
 
 `ifdef SDF
-    initial $sdf_annotate(`SDFFILE, u_SME);
+    initial $sdf_annotate(`SDFFILE, u_geofence);
 `endif
 
 always begin #(`CYCLE/2) clk = ~clk; end
 
 initial begin
-    $fsdbDumpfile("SME.fsdb");
-    $fsdbDumpvars;
+    $fsdbDumpfile("geofence.fsdb");
+    $fsdbDumpvars();
     $fsdbDumpMDA;
 end
 
 //initial begin
-//    $dumpfile("SME.vcd");
+//    $dumpfile("geofence.vcd");
 //    $dumpvars;
 //end
 
@@ -53,9 +48,9 @@ initial begin
     $display("----------------------");
     $display("-- Simulation Start --");
     $display("----------------------");
-    @(posedge clk); #1; reset = 1'b1; 
+    @(posedge clk);  #2 reset = 1'b1; 
     #(`CYCLE*2);  
-    @(posedge clk); #1;   reset = 1'b0;
+    @(posedge clk);  #2  reset = 1'b0;
 end
 
 reg [22:0] cycle=0;
@@ -66,183 +61,93 @@ always @(posedge clk) begin
         $display("--------------------------------------------------");
         $display("-- Failed waiting valid signal, Simulation STOP --");
         $display("--------------------------------------------------");
-        $fclose(linedata);
+        $fclose(fd);
         $finish;
     end
 end
 
 initial begin
-    linedata = $fopen(`PAT,"r");
-    if (linedata == 0) begin
+    fd = $fopen(`PAT,"r");
+    if (fd == 0) begin
         $display ("pattern handle null");
         $finish;
     end
 end
 
-integer strindex;
-reg [7:0] goldmatch_index;
-reg  goldmatch;
 reg  valid_reg;
-reg wait_valid;
-reg  getmatch;
-reg [7:0] getmatch_index;
 always @(posedge clk) begin
     valid_reg = valid;
 end
+reg wait_valid;
+reg get_inside;
+integer ap_num;
 
-integer score=0;
-integer allpass=1;
-always @(negedge clk) begin
+always @(posedge clk ) begin
     if (reset) begin
         wait_valid=0;
     end
     else begin
-        if (wait_valid==1 && valid ==1) begin
-            wait_valid=0;
-            getmatch=match;
-            getmatch_index=match_index;
-            //$display ("       string/pattern = %s/%s",strnum,patnum);
-            if(goldmatch==1)begin
-                if(getmatch == goldmatch) begin
-                    score++;
-                    if(getmatch_index==goldmatch_index) begin
-                        score=score+2;
-                        //colordisplay(cycle,goldmatch,goldmatch_index,getmatch,getmatch_index,3);
-                        $display ("       cycle %2h, expect(%h,%h) , get(%h,%h) >> Pass",cycle,goldmatch,goldmatch_index,getmatch,getmatch_index);
-                    end
-                    else begin 
-                        allpass=0;
-                        //colordisplay(cycle,goldmatch,goldmatch_index,getmatch,getmatch_index,2);
-                        $display ("       cycle %2h, expect(%h,%h) , get(%h,%h) >> Wrong index",cycle,goldmatch,goldmatch_index,getmatch,getmatch_index);
-                    end
+        if(wait_valid == 0) begin
+            if(ap_num ==6) wait_valid =1;
+        end
+        else begin
+            if (valid ==1) begin
+                wait_valid=0;
+                get_inside=is_inside;
+                if(get_inside == obj_isin) begin
+                    pass = pass +1;
+                    $display("Object%0d: Golde/Return => %0d/%d, PASS\n",objnum,obj_isin,get_inside);
                 end
                 else begin
-                    allpass=0;
-                    //colordisplay(cycle,goldmatch,goldmatch_index,getmatch,getmatch_index,0);
-                    $display ("       cycle %2h, expect(%h,%h) , get(%h,%h) >> Fail",cycle,goldmatch,goldmatch_index,getmatch,getmatch_index);
+                    fail = fail +1;
+                    $display("Object%0d: Golde/Return => %0d/%d, FAIL\n",objnum,obj_isin,get_inside);
                 end
             end
-            else begin
-                if(getmatch == goldmatch) begin
-                    score++;
-                    //colordisplay(cycle,goldmatch,goldmatch_index,getmatch,getmatch_index,3);
-                    $display ("       cycle %2h, expect(%h,--) , get(%h,--) >> Pass",cycle, goldmatch,getmatch);
-                end
-                else begin
-                    allpass=0;
-                    //colordisplay(cycle,goldmatch,goldmatch_index,getmatch,getmatch_index,1);
-                    $display ("       cycle %2h, expect(%h,--) , get(%h,--) >> Fail",cycle, goldmatch,getmatch);
-                end
-            end
-        //$display ("       score = %d",score);
         end
     end
 end
 
 always @(negedge clk ) begin
     if (reset) begin
-        isstring=0;
-        ispattern=0;
-        valid_reg=0;
+        X=0;
+        Y=0;
+        ap_num = 0;
     end 
     else begin
-        if (strindex==strdata.len()-1) begin
-            isstring=0;
-            if(ispattern==1) wait_valid=1;
-            ispattern=0;
-        end
-        if (wait_valid == 1'b0) begin
-            if (strindex<strdata.len()-1) begin
-                strindex=strindex+1;
-                chardata = strdata.getc(strindex);
-                //$display("%h %s,%h",cycle,chardata,chardata);
-            end 
-            else begin
-                if (!$feof(linedata))begin
-                    char_count = $fgets (data, linedata);
-                    if (char_count !== 0) begin
-                        if(data.substr(0,3) == "str:") begin
-                            isstring=1; 
-                            strindex=0;
-                            strnum = data.substr(4,4);
-                            strdata = data.substr(6,data.len()-2);
-                            $display("  __________________________________________________________");
-                            $display("  == String %s  \"%s\"", strnum, strdata);
-                            chardata = strdata.getc(strindex);
-                            //$display("%h %s,%h",cycle,chardata,chardata);
-                        end 
-                        else begin
-                            if(data.substr(0,3) == "pat:") begin
-                                ispattern=1; 
-                                strindex=0;
-                                patnum = data.substr(4,4);
-                                goldmatch_s = data.substr(6,6);
-                                goldmatch = goldmatch_s.atoi();
-                                goldmatch_index_s = data.substr(8,9);
-                                goldmatch_index = goldmatch_index_s.atoi();
-                                strdata = data.substr(11,data.len()-2);
-                                //$display("==== Pattern__%s__%b__%2d__%s", patnum, goldmatch,goldmatch_index,strdata);
-                                $display("  -- Pattern %s  \"%s\"", patnum, strdata);
-                                chardata = strdata.getc(strindex);
-                                //$display("%h %s,%h",cycle,chardata,chardata);
-                            end
-                        end
+        if (!$feof(fd)) begin
+            if(wait_valid ==0) begin
+                charcount = $fgets (line, fd);
+                if(charcount != 0) begin
+                    while( line.substr(1, 2) == "//") charcount = $fgets (line, fd);
+                    if( line.substr(0, 5) == "object") begin
+                        charcount = $sscanf(line, "object %d %d",objnum,obj_isin);
+                        if(obj_isin == 1)
+                            $display ("Scenario%0d(in):     X     Y",objnum);
+                        else
+                            $display ("Scenario%0d(out):    X     Y",objnum);
+                        ap_num=0;
+                        charcount = $fgets (line, fd);
+                        charcount = $sscanf(line, "%d %d",X,Y);
+                        $display("        Object:  %d, %d", X ,Y);
+                    end 
+                    else begin
+                        ap_num = ap_num+1;
+                        charcount = $sscanf(line, "%d %d",X,Y);
+                        $display("           AP%1d:  %d, %d",ap_num, X ,Y);
                     end
                 end
-                else begin
-                    $display("----------------------------------");
-                    if(allpass == 1 && score >1)
-                    $display("-- Simulation finish, ALL PASS  --");
-                    else
-                    $display("-- Simulation finish            --");
-                    $display("-- cycle =%3d , Score =%2d       --",cycle, score);
-                    $display("----------------------------------");
-                    $fclose(linedata);
-                    $finish;
-                end
             end
-        end // if wait_valid == 0
+        end //if (!$feof(fd)) begin
+        else begin
+             $fclose(fd);
+             $display ("-------------------------------------------------");
+             if(pass == 50 && fail == 0)
+                 $display("--    Simulation finish,  ALL PASS             --");
+             else
+                 $display("-- Simulation finish,  Pass = %2d , Fail = %2d   --",pass,fail);
+             $display ("-------------------------------------------------");
+             $finish;
+        end
     end
 end
 endmodule
-
-task colordisplay;
-    input [12:0] cycle;
-    input goldmatch;
-    input [7:0] goldmatchindex;
-    input getmatch;
-    input [7:0] getmatchindex;
-    input [1:0]pass; 
-    begin
-        $write ("       cycle %2h, expect(%h,%h) , get(",cycle,goldmatch,goldmatchindex);
-        case(pass)
-            2'd0: begin   // expect match , failed
-                $write("%c[1;31m",27);
-                $write ("%h,%h",getmatch,getmatchindex);
-                $write("%c[0m",27);
-                $write (") >> Fail\n");
-
-                  end
-            2'd1: begin   // expect unmatch, failed
-                $write("%c[1;31m",27);
-                $write ("%h",getmatch);
-                $write ("%c[0m",27);
-                $write (",%h) >> Fail\n",getmatchindex);
-
-                  end
-            2'd2: begin  // expect match, wrong index
-                $write ("%h,",getmatch);
-                $write("%c[1;31m",27);
-                $write ("%h",getmatchindex);
-                $write("%c[0m",27);
-                $write (") >> Wrong index\n");
-
-                  end
-            2'd3: begin // both match or unmatch, passed
-                $write ("%h,%h",getmatch,getmatchindex);
-                $write (") >> Pass\n");
-
-                  end
-        endcase
-    end
-endtask
